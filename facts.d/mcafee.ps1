e737166a05c98a9bd334864a276b0f3c72179ae5
Chris Barker <cbarker@puppetlabs.com>
2014-11-09 10:31:43 -0800

    fixes fact options

--- facts.d/mcafee.ps1
+++ facts.d/mcafee.ps1
@@ -1,6 +1,11 @@
 $key = 'HKLM:\Software\McAfee\AVEngine'
-$AVDatVersion = (Get-ItemProperty -Path $key -Name AVDatVersion).AVDatVersion
-$AVDatDate = (Get-ItemProperty -Path $key -Name AVDatDate).AVDatDate
 
-Write-Host McAfeeDatVersion=$AVDatVersion
-Write-Host McAfeeDatDate=$AVDatDate
+$AVDatVersion, $AVDatDate = 'N/A', 'N/A'
+if (Test-Path $key)
+{
+  $AVDatVersion = (Get-ItemProperty -Path $key -Name AVDatVersion).AVDatVersion
+  $AVDatDate = (Get-ItemProperty -Path $key -Name AVDatDate).AVDatDate
+}
+
+Write-Output "McAfeeDatVersion=$AVDatVersion"
+Write-Output "McAfeeDatDate=$AVDatDate"
